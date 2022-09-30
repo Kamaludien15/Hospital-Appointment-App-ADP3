ec47dc0b045ce8d18a2956076053dacaeebf6c77
Rhegan Albert Fortuin <kingrhegan1234@gmail.com>
2022-09-30 08:24:59 +0200

    adding Patient, Gender and PatientGender services and repository

---
+++ src/main/java/za/ac/cput/repository/IPatientRepository.java
@@ -0,0 +1,10 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Patient;
+
+@Repository
+public interface IPatientRepository extends JpaRepository<Patient, String> {
+    public Patient findByPatientID(String id);
+}
