dd29fcef8ec7a0dd485ac223c05d204da68c476b
Rhegan Albert Fortuin <kingrhegan1234@gmail.com>
2022-10-01 09:39:20 +0200

    gender, patient and patientGender controller

--- src/main/java/za/ac/cput/controller/GenderController.java
+++ src/main/java/za/ac/cput/controller/GenderController.java
@@ -1,4 +1,55 @@
 package za.ac.cput.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.api.AppointmentAPI;
+import za.ac.cput.api.GenderAPI;
+import za.ac.cput.domain.Appointment;
+import za.ac.cput.domain.Gender;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/hospital_appointment_management-db/gender")
+@Slf4j
 public class GenderController {
+    private GenderAPI api;
+
+    @Autowired
+    public GenderController(GenderAPI api) {
+        this.api = api;
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Gender> save(@Valid @RequestBody Gender gender){
+        log.info("Save request: {}", gender);
+        Gender response = this.api.save(gender);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("read/{gender_id}")
+    public ResponseEntity<Gender> read(@PathVariable String id){
+        log.info("Read request: {}", id);
+        Gender response = this.api.read(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Gender not found"));
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("delete/{gender}")
+    public ResponseEntity<Void> delete(@PathVariable Gender gender){
+        log.info("Delete request: {}", gender);
+        this.api.delete(gender);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Gender>> getAll(){
+        List<Gender> response = this.api.getAll();
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/za/ac/cput/controller/PatientController.java
+++ src/main/java/za/ac/cput/controller/PatientController.java
@@ -1,4 +1,56 @@
 package za.ac.cput.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.api.AppointmentAPI;
+import za.ac.cput.api.PatientAPI;
+import za.ac.cput.domain.Appointment;
+import za.ac.cput.domain.Patient;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/hospital_appointment_management-db/patient")
+@Slf4j
 public class PatientController {
+
+    private PatientAPI api;
+
+    @Autowired
+    public PatientController(PatientAPI api) {
+        this.api = api;
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Patient> save(@Valid @RequestBody Patient patient){
+        log.info("Save request: {}", patient);
+        Patient response = this.api.save(patient);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("read/{patient_id}")
+    public ResponseEntity<Patient> read(@PathVariable String id){
+        log.info("Read request: {}", id);
+        Patient response = this.api.read(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient not found"));
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("delete/{patient}")
+    public ResponseEntity<Void> delete(@PathVariable Patient patient){
+        log.info("Delete request: {}", patient);
+        this.api.delete(patient);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Patient>> getAll(){
+        List<Patient> response = this.api.getAll();
+        return ResponseEntity.ok(response);
+    }
 }
